95b86b040887ca8aab18a20706300bd1b6f37620
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2025-04-09 22:44:22 +0300

    c

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,45 @@ import chunkFile from "./util/chunkFile.js";
 
 export interface ChunkOptions {
     /**
-     * The size of each chunk in kilobytes
+     * @brief The size of each chunk in kilobytes
      * @default 1024
      * @minimum 1
     */
     chunkSize: number;
     /**
-     * The path to the file to be chunked
+     * @brief The path to the file to be chunked
      * @format local or absolute path
     */
     filePath: string;
     /**
-     * The directory where the chunks will be saved
+     * @brief The directory where the chunks will be saved
      * @format local or absolute path
     */
     outputDir: string;
+    /**
+     * @brief A function that will be called every time a chunk is created
+     * @param chunk - The number of the chunk that was created
+     * @param totalChunks - The total number of chunks that will be created
+    */
+    onProgress?: (chunkNumber: number, totalChunks: number, chunkData: Buffer) => void | Promise<void>;
+    /**
+     * Whether or not to pass the chunk data to the onProgress callback and not to the disk
+     * @default false
+    */
+    passChunkToCallback?: boolean;
 }
 
 /**
  * Chunks a file into smaller pieces
  */
-export default async ({
-    chunkSize = 1024,
-    filePath,
-    outputDir
-}: ChunkOptions) => {
-    filePath = await verifyFilePath(filePath);
-    if (filePath.length === 0) return;
-    outputDir = await verifyFolder(outputDir);
-    if (outputDir.length === 0) return;
-    await chunkFile({ chunkSize, filePath, outputDir });
+export default async function td (opts: ChunkOptions)  {
+    opts.filePath = await verifyFilePath(opts.filePath);
+    if (opts.filePath.length === 0) return;
+    if (!opts.passChunkToCallback) {
+        opts.outputDir = await verifyFolder(opts.outputDir);
+        if (opts.outputDir.length === 0) return;
+    }
+    await chunkFile(opts);
 };
+
+export {getFileChunkStats} from "./util/chunkFile.js"
--- src/util/chunkFile.ts
+++ src/util/chunkFile.ts
@@ -1,7 +1,7 @@
 import _error from "./io/_error.js";
 import { ChunkOptions } from "../index.js";
 import fsp from "fs/promises";
-import fs from "fs";
+import fs, { Stats } from "fs";
 
 function calculateReadSize(
     totalSizeKB: number,
@@ -16,20 +16,37 @@ function calculateReadSize(
     return chunkSizeKB;
   }
 
-export default async function chunkFile({ chunkSize, filePath, outputDir }: ChunkOptions): Promise<void> {
+/**
+ * Calculates the number of chunks and the stats of a file
+ * @param filePath - The path to the file (local or absolute path)
+ * @param chunkSize (in KB) - The size of each chunk in kilobytes
+ */
+export async function getFileChunkStats(filePath: string, chunkSize: number): Promise<{stats: Stats, chunks: number}| null> {
     try {
         const stats = await fsp.stat(filePath);
         const chunks = Math.ceil(stats.size / (chunkSize * 1024));
+        return { stats, chunks };
+    } catch (error) {
+        _error("Unable to get file chunk stats. Error: " + String(error))
+        return null;
+    }
+}
+
+export default async function chunkFile({ chunkSize, filePath, outputDir, onProgress,passChunkToCallback = false }: ChunkOptions): Promise<void> {
+    try {
+        const chunkStats = await getFileChunkStats(filePath, chunkSize);
+        if (!chunkStats) return;
+        const { stats, chunks } = chunkStats;
         const file = await fsp.open(filePath, "r");
         let progressKB = 0;
         for (let i = 0; i < chunks; i++) {
             const readSize = calculateReadSize(stats.size / 1024, chunkSize, i);
-            console.log(readSize)
             const buffer = Buffer.alloc(readSize * 1024);
             await file.read(buffer, 0, readSize * 1024, i * chunkSize * 1024);
-            const chunkPath = `${outputDir}/chunk-${i + 1}.chunk`;
-            await fsp.writeFile(chunkPath, buffer);
+            const chunkPath = `${outputDir}/${i}.chunk`;
+            if (!passChunkToCallback) await fsp.writeFile(chunkPath, buffer);
             progressKB += readSize;
+            if (onProgress) await onProgress(i, chunks, passChunkToCallback ? buffer : Buffer.alloc(0));
         }
     } catch (error) {
         _error("Unable to chunk file. Error: " + String(error))
